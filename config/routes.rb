f337d550561b378dd63cebbd367bfc14d9ba5604
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2013-06-10 16:39:26 -0300

    Adjusting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Raffler::Application.routes.draw do
   scope "api" do
-    resources :entries
+    resources :entries, except: [:new, :edit]
   end
 
   root to: "main#index"
-  match '*path', to: 'main#index'
+  # match '*path', to: 'main#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
